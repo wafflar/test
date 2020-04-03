1b2d5cb320d929ffefbbdf42cbd0fb74650c75c5
Yoshiaki <wafflarlangel00@gmail.com>
2020-04-03 10:18:25 +0900

    Initial sharing of project

---
+++ MyClass.java
@@ -0,0 +1,35 @@
+
+/**
+ * クラス MyClass の注釈をここに書きます.
+ * 
+ * @author (あなたの名前)
+ * @version (バージョン番号もしくは日付)
+ */
+public class MyClass
+{
+    // インスタンス変数 - コードに合わせて説明を書き換えます.
+    private int x;
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+    /**
+     * MyClass クラスのインスタンスのためのコンストラクタ
+     */
+    public MyClass()
+    {
+        // インスタンス変数の初期化
+        x = 0;
+    }
+
+    /**
+     * メソッドの例 - メソッドとともにこのコメントも置き換えてください.
+     * 
+     * @param  y    メソッドのためのサンプル引数
+     * @return        x と y の和
+     */
+    public int sampleMethod(int y)
+    {
+        // ここにあなたのコードを書きます
+        return x + y;
+    }
+}
